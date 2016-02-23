fed86e0471ed764d3908481253b9527e69df233f
Aki Rose Braun <hi@akiro.se>
2016-02-23 01:58:23 -0800

    debugs ghpages deploy script?

--- deploy-ghpages.sh
+++ deploy-ghpages.sh
@@ -3,13 +3,13 @@ cd dist || exit 0;
 
 if  [ "$TRAVIS" = "true" -a "$TRAVIS_PULL_REQUEST" = "false"  -a "$TRAVIS_BRANCH" = "production" ]
   then
-    (
-    cp ../CNAME ./CNAME
-    git init
+    (cp ../CNAME ./CNAME
+     git init
      git config user.name "Travis-CI"
      git config user.email "travis@example.org"
      git add .
      git commit -m "Deployed to Github Pages"
-     git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
+     git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages
+#     git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
     )
 fi
